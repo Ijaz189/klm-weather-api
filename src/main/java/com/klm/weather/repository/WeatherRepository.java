65e73914a0f1c27dc32b9f8d1b306c7829de8622
Ijaz189 <83556139+Ijaz189@users.noreply.github.com>
2025-04-30 17:03:45 +0200

    Update WeatherRepository.java
    Added comments above queries as per their fetching criteria

--- src/main/java/com/klm/weather/repository/WeatherRepository.java
+++ src/main/java/com/klm/weather/repository/WeatherRepository.java
@@ -12,28 +12,35 @@ import com.klm.weather.model.Weather;
 
 @Repository
 public interface WeatherRepository extends JpaRepository<Weather, Integer> {
-
+	// Retrieves all weather records ordered by ID in ascending order
 	@Query("SELECT w FROM Weather w ORDER BY w.id ASC")
 	List<Weather> findAllOrderById();
-
+	
+	// Retrieves all weather records matching the given date by ID in ascending order 
 	@Query("SELECT w FROM Weather w WHERE w.date = :date ORDER BY w.id ASC")
 	List<Weather> findByDate(@Param("date") Date date);
 
+	// Retrieves all weather records matching the given city by ID in ascending order
 	@Query("SELECT w FROM Weather w WHERE LOWER(w.city) IN :cities ORDER BY w.id ASC")
 	List<Weather> findByCityIgnoreCaseIn(@Param("cities") List<String> cities);
 
+	// Retrieves all weather records matching cities for  by ID in ascending order
 	@Query("SELECT w FROM Weather w WHERE w.date = :date AND LOWER(w.city) IN :cities ORDER BY w.id ASC")
 	List<Weather> findByDateAndCities(@Param("date") Date date, @Param("cities") List<String> cities);
 
+	// Retrieves all weather records order by ascending date and if same date then order by ascending id
 	@Query("SELECT w FROM Weather w ORDER BY w.date ASC, w.id ASC")
 	List<Weather> findAllOrderByDateAsc();
 
+	// Retrieves all weather records order by descending date and if same date then order by ascending id
 	@Query("SELECT w FROM Weather w ORDER BY w.date DESC, w.id ASC")
 	List<Weather> findAllOrderByDateDesc();
 
+	// Retrieves all weather records for matching date with order by ascending date and if same date then order by ascending id
 	@Query("SELECT w FROM Weather w WHERE w.date = :date ORDER BY w.date ASC, w.id ASC")
 	List<Weather> findByDateOrderByDateAsc(@Param("date") Date date);
-
+	
+	// Retrieves all weather records for matching date with order by descending date and if same date then order by ascending id
 	@Query("SELECT w FROM Weather w WHERE w.date = :date ORDER BY w.date DESC, w.id ASC")
 	List<Weather> findByDateOrderByDateDesc(@Param("date") Date date);
 }
